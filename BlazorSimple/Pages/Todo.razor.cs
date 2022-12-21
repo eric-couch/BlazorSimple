588f7f88b5c298f0b568bb29763ffa42bb416e9a
Eric Couch <ericcouch@yahoo.com>
2022-12-21 11:00:27 -0600

    Add project files.

---
+++ BlazorSimple/Pages/Todo.razor.cs
@@ -0,0 +1,17 @@
+﻿using BlazorSimple.Shared;
+
+namespace BlazorSimple.Pages
+{
+    public partial class Todo
+    {
+        private List<TodoItem> todos = new List<TodoItem>();
+        private string newTodo = "";
+
+        private async void AddTodo()
+        {
+            todos.Add(new TodoItem { Title = newTodo });
+            await localStorage.SetItemAsync("todo", newTodo);
+            newTodo = String.Empty;
+        }
+    }
+}
